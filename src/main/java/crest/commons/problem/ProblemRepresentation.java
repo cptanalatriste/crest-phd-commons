5b80e39e30ed1b0ac63211dfff6af673a92c4bde
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-02-11 17:35:12 +0000

    Template code

---
+++ src/main/java/crest/commons/problem/ProblemRepresentation.java
@@ -0,0 +1,13 @@
+package crest.commons.problem;
+
+import java.nio.file.Path;
+
+public class ProblemRepresentation {
+
+  public ProblemRepresentation(Path filePath) {
+
+  }
+
+
+
+}
---
+++ src/main/java/crest/commons/solution/SolutionRepresentation.java
@@ -0,0 +1,14 @@
+package crest.commons.solution;
+
+import java.nio.file.Path;
+
+public class SolutionRepresentation {
+
+  public String getSolutionAsString() {
+    return "";
+  }
+
+  public void generateProgramFile(Path filePath) {
+
+  }
+}
