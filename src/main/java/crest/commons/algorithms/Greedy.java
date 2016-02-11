9b2b4892261f543acb3fcfe9360b9a48418483c2
Fan <>
2016-02-11 19:08:24 +0000

    add algorithm package

---
+++ src/main/java/crest/commons/algorithms/Greedy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crest.commons.algorithms;
+
+/**
+ *
+ * @author J
+ */
+public class Greedy {
+    
+}
