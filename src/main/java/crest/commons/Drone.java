54a7ab266771f8affe9d79515f32ff5edc4f79cf
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-02-11 19:39:54 +0000

    Supporting drone load

--- src/main/java/crest/commons/Drone.java
+++ src/main/java/crest/commons/Drone.java
@@ -2,43 +2,63 @@ package crest.commons;
 
 import crest.commons.solution.Command;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Drone {
 
   private final int identifier;
   private int xPos;
   private int yPos;
-  
+
   private List<Command> commands;
-  
-  public Drone(int id, int x, int y){
-	  identifier = id;
-	  setYPos(x);
-	  setYPos(y);
+  private Map<Integer, Integer> products = new HashMap<Integer, Integer>();
+
+  public Drone(int id, int x, int y) {
+    identifier = id;
+    setYPos(x);
+    setYPos(y);
   }
 
-public int getId(){
-	return getIdentifier();
-}
-  
-public int getIdentifier() {
-	return identifier;
-}
+  public int getId() {
+    return getIdentifier();
+  }
 
-public int getXPos() {
-	return xPos;
-}
+  public int getIdentifier() {
+    return identifier;
+  }
 
-public void setXPos(int xPos) {
-	this.xPos = xPos;
-}
+  public int getXPos() {
+    return xPos;
+  }
 
-public int getYPos() {
-	return yPos;
-}
+  public void setXPos(int xPos) {
+    this.xPos = xPos;
+  }
 
-public void setYPos(int yPos) {
-	this.yPos = yPos;
-}
+  public int getYPos() {
+    return yPos;
+  }
+
+  public void setYPos(int yPos) {
+    this.yPos = yPos;
+  }
+
+  public List<Command> getCommands() {
+    return commands;
+  }
+
+  public void setCommands(List<Command> commands) {
+    this.commands = commands;
+  }
+
+  public Map<Integer, Integer> getProducts() {
+    return products;
+  }
+
+  public void setProducts(Map<Integer, Integer> products) {
+    this.products = products;
+  }
+
 }
