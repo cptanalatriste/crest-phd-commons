a74ca87feb831b8cfb9187e796b981a060b8db11
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-02-11 19:04:08 +0000

    Drone additional information

--- src/main/java/crest/commons/Drone.java
+++ src/main/java/crest/commons/Drone.java
@@ -8,4 +8,39 @@ public class Drone {
 
   private int identifier;
   private List<Command> commands;
+  private int currentPositionX;
+  private int currentPositionY;
+
+  public int getIdentifier() {
+    return identifier;
+  }
+
+  public void setIdentifier(int identifier) {
+    this.identifier = identifier;
+  }
+
+  public List<Command> getCommands() {
+    return commands;
+  }
+
+  public void setCommands(List<Command> commands) {
+    this.commands = commands;
+  }
+
+  public int getxCoord() {
+    return currentPositionX;
+  }
+
+  public void setCurrentPositionX(int currentPositionX) {
+    this.currentPositionX = currentPositionX;
+  }
+
+  public int getyCoord() {
+    return currentPositionY;
+  }
+
+  public void setCurrentPositionY(int currentPositionY) {
+    this.currentPositionY = currentPositionY;
+  }
+
 }
