8838a707a20c0c93a9d7062373a8929d1ea3c81f
Bobby R. Bruce <r.bruce@cs.ucl.ac.uk>
2016-02-11 18:27:57 +0000

    Adding modified WareHouse for Carlos

--- src/main/java/crest/commons/WareHouse.java
+++ src/main/java/crest/commons/WareHouse.java
@@ -1,5 +1,26 @@
 package crest.commons;
 
 public class WareHouse {
+	private final int id;
+	private final int xCoord;
+	private final int yCoord;
+	
+	public WareHouse(int warehouseId, int warehouseX, int warehouseY){
+		id = warehouseId;
+		xCoord = warehouseX;
+		yCoord = warehouseY;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getxCoord() {
+		return xCoord;
+	}
+
+	public int getyCoord() {
+		return yCoord;
+	}
 
 }
